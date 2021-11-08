a10058a4d6d349c3c69962f3b957d8842bcb7764
Daimon-J <daimon.java.best@gmail.com>
2021-11-08 10:45:28 +0200

    6.0.3. Массивы и цикл for. [#226665]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,9 +10,13 @@ public class ArrayDefinition {
         names[1] = "Linux";
         names[2] = "Lee";
         names[3] = "Marina";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
         System.out.println(ages.length);
         System.out.println(surname.length);
         System.out.println(prices.length);
+
     }
 
 }
