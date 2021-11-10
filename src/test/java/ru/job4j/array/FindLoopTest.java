c954e9d0a9624c3ea1ba9cfe0f0c69ac7ca0f450
Daimon-J <daimon.java.best@gmail.com>
2021-11-10 12:09:56 +0200

    6.5. Поиск индекса в диапазоне. [#156318 #194875]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -6,29 +6,40 @@ import org.junit.Test;
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0(){
-        int[] date = {5,4,3,2};
+    public void whenArrayHas5Then0() {
+        int[] date = {5, 4, 3, 2};
         int el = 5;
-        int result = FindLoop.indexOF(date, el);
+        int result = FindLoop.indexOf(date, el);
         int expected = 0;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenArrayHas9Then1(){
-        int[] date = {2,9,1,1,2};
+    public void whenArrayHas9Then1() {
+        int[] date = {2, 9, 1, 1, 2};
         int el = 9;
-        int result = FindLoop.indexOF(date, el);
+        int result = FindLoop.indexOf(date, el);
         int expected = 1;
         Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void whenArrayHas9ThenNot(){
-        int[] date = {2,9,1,1,2};
+    public void whenArrayHas9ThenNot() {
+        int[] date = {2, 9, 1, 1, 2};
         int el = 22;
-        int result = FindLoop.indexOF(date, el);
+        int result = FindLoop.indexOf(date, el);
         int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
         Assert.assertEquals(expected, result);
     }
 }
